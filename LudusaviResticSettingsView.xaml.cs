3a21a615b99261fc0d57f4d9903cddf3c56f7d4d
sharkusmanch <9328629+sharkusmanch@users.noreply.github.com>
2021-08-09 13:01:05 -0700

    Initial

---
+++ LudusaviResticSettingsView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Playnite.SDK;
+using Playnite.SDK.Plugins;
+using Playnite.SDK.Models;
+
+namespace LudusaviRestic
+{
+    public partial class LudusaviResticSettingsView : UserControl
+    {
+        private LudusaviRestic plugin;
+
+        public LudusaviResticSettingsView(LudusaviRestic plugin)
+        {
+            InitializeComponent();
+            this.plugin = plugin;
+        }
+
+        public void OnBrowseLudusaviExecutablePath(object sender, RoutedEventArgs e)
+        {
+            var choice = this.plugin.PlayniteApi.Dialogs.SelectFile("Executable|*.exe");
+
+            if (choice.Length > 0)
+            {
+                this.plugin.settings.LudusaviExecutablePath = choice;
+            }
+        }
+
+        public void OnBrowseResticExecutablePath(object sender, RoutedEventArgs e)
+        {
+            var choice = this.plugin.PlayniteApi.Dialogs.SelectFile("Executable|*.exe");
+
+            if (choice.Length > 0)
+            {
+                this.plugin.settings.ResticExecutablePath = choice;
+            }
+        }
+
+        public void OnBrowseRcloneConfPath(object sender, RoutedEventArgs e)
+        {
+            var choice = this.plugin.PlayniteApi.Dialogs.SelectFile("Conf|*.conf");
+
+            if (choice.Length > 0)
+            {
+                this.plugin.settings.RcloneConfigPath = choice;
+            }
+        }
+    }
+}
